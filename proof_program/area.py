836d680760bd39a6feb0909dea7a83b1579db21e
andersonwu2000 <andersonwu2000@gmail.com>
2021-06-30 08:41:48 +0800

    Update area.py

--- proof_program/area.py
+++ proof_program/area.py
@@ -3,12 +3,12 @@ class area:
     def __init__(self, bases=set(), axioms=set()) -> None:
         """
         Parameters
-            bases (set of areas): 該領域的基礎領域
-            axioms  (set of str): 該領域的所有公理或基本假設
+            bases (set of areas): 該 area 的基礎 area 
+            axioms  (set of str): 該 area 的所有公理或基本假設
         Variables
-            self.theorems (dict of str&AST): 該領域的所有定理
-            self.bases       (set of areas): 該領域的基礎領域
-            self.ancestor    (set of areas): 該領域的所有基本領域
+            self.theorems (dict of str&AST): 該 area 的所有定理
+            self.bases       (set of areas): 該 area 的基礎 area 
+            self.ancestor    (set of areas): 該 area 的所有基本 area 
         """
         self.theorems = self.syntax_analysis(axioms)
         if type(bases) is area:
@@ -20,28 +20,28 @@ class area:
         self.ancestor = self.search_bases()
     
     def __call__(self, content:str, check_bases=True) -> bool:
-        """ 檢查敘述對於該領域是否正確 """
-        # 對該領域的所有定理進行檢查
+        """ 檢查敘述對於該 area 是否正確 """
+        # 對該 area 的所有定理進行檢查
         for thm in self.theorems:
             if self.logical(thm, content):
                 return True
         if not check_bases:
             return False
-        # 對所有基本領域的定理進行檢查
+        # 對所有基本 area 的定理進行檢查
         for bases in self.ancestor:
             if bases(content, False):
                 return True
         return False
 
     # def add(self, theorems) -> dict:
-    #     """ 對該領域添加新的定理 """
+    #     """ 對該 area 添加新的定理 """
     #     if type(theorems) is str:
     #         self.theorems.add(theorems)
     #     elif type(theorems) in {list, tuple, set}:
     #         self.theorems.update(theorems)
 
     def search_bases(self) -> None:
-        """ 收集所有基本領域 """
+        """ 收集所有基本 area  """
         ancestor = self.bases
         for base in self.bases:
             ancestor = ancestor.union(base.search_bases())
